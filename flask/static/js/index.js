7b1ae7558b9a199f540540c7e6a89f1f2a7c2314
CHAN KAI CHENG <106880309+JeterChan@users.noreply.github.com>
2023-06-26 13:52:45 +0800

    update index js
    更改star抓取的class，用news_id產生唯一性

--- flask/static/js/index.js
+++ flask/static/js/index.js
@@ -1,19 +1,27 @@
 function getRating(){
-    var  stars = document.querySelectorAll(".stars i");
+    var  stars = document.querySelectorAll(".{{news_id}} p");
     console.log(stars);
-    var ratingResult = document.getElementById("stars-score");
+    var ratingResult = document.querySelector(".{{news_id}} span");
     printRatingResult(ratingResult);
     stars.forEach((star,index1)=> {
-        star.addEventListener("click",() => {        
+        star.onclick = function() {
+            let current_star_level = index1+1;
+            console.log(index1+1)
             stars.forEach((star,index2) => {
-                index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-                console.log(index2);
+                console.log(index2)
+                if(current_star_level >= index2+1 )
+                {
+                    star.innerHTML = '&#9733';
+                } else{
+                    star.innerHTML = '&#9734';
+                }
             });
             printRatingResult(ratingResult,index1+1);
             // 取最後一個 index1 為評分的值 需要+1 因為index從0開始計算
-        });
-    });
+        };
+    })
 }
+
 
 function printRatingResult(result,num=0){
     result.textContent = `${num}/5`;
