eeba8c9d16fff37c12889ace137222ca109745dd
chickenmaru <105425398+chickenmaru@users.noreply.github.com>
2023-07-26 19:03:14 +0800

    Update kmeans.py
    0726 修正output新聞重複的bug

--- recommandtion0714/kmeans.py
+++ recommandtion0714/kmeans.py
@@ -8,6 +8,7 @@ from sklearn.cluster import KMeans
 import random
 import pandas as pd
 import os
+import json
 
 current_dir = os.getcwd()
 
@@ -18,6 +19,20 @@ stopwords_name = "stop_words.txt"
 stopwords_path = data_path+stopwords_name
 
 CLSTER_NUM = 50
+
+with open('news.json', 'r', encoding='utf-8') as jsonFile:
+        f = jsonFile.read()
+        a = json.loads(f) 
+        df = pd.json_normalize(json.loads(f))
+
+with open('news.json', 'r', encoding='utf-8') as jsonFile:
+        f = jsonFile.read()
+        a = json.loads(f) 
+        df = pd.json_normalize(json.loads(f))
+
+        # 提取'id'字段的值并存储在列表中
+        news_summary_list = df['summary'].tolist()
+        news_id_list = df['_id.$oid'].tolist()
 
 class KmeansClustering():
     def __init__(self, stopwords_path=stopwords_path):
@@ -89,7 +104,6 @@ class KmeansClustering():
 
         # 用来评估簇的个数是否合适,距离约小说明簇分得越好,选取临界点的簇的个数
         # score = clf.inertia_
-
         # 每个样本所属的簇
         result = {}
         for text_idx, label_idx in enumerate(y):
@@ -141,3 +155,86 @@ def run_kmeans(news_summary,cluster_num):
         finished_kmeans_summary.append(news_summary[news_index])
     return finished_kmeans_summary
 
+
+def run_kmeans_from_df(df,cluster_num):
+    print(df)
+    # 提取'id'字段的值并存储在列表中
+    news_summary_list = df['summary'].tolist()
+    news_id_list = df['_id.$oid'].tolist()
+    
+    with open(data_path+'kmeans.txt', "w",encoding='utf-8') as file:
+        # 遍历列表，逐行写入文件
+        for item in news_summary_list:
+            file.write(item+"\n")
+    CLSTER_NUM = cluster_num
+
+    Kmeans = KmeansClustering(stopwords_path=stopwords_path)
+    result = Kmeans.kmeans(data_path+'kmeans.txt', n_clusters=cluster_num)
+    
+    df_news_summary = translate_text_to_dataframe('kmeans.txt')
+
+    output_news_list = choose_final_news(result,df_news_summary,cluster_num)
+    finished_kmeans_summary_list = []
+
+    for news_index in (output_news_list):
+        finished_kmeans_summary_list.append(news_summary_list[news_index])
+
+    df_return = pd.DataFrame()
+
+    for summary in finished_kmeans_summary_list:
+        selected_rows = df[df["summary"] == summary]
+        print(len(selected_rows))
+        if len(selected_rows)==1:
+            df_return = df_return.append(selected_rows, ignore_index=True) 
+        if len(selected_rows)>1:
+            print("有兩個以上一樣的")
+            selected_rows = selected_rows.iloc[0]
+            df_return = df_return.append(selected_rows, ignore_index=True)
+    
+        
+ 
+    print(df_return['title']) 
+    
+    
+    return df_return['summary']
+
+"""
+import scipy
+from  scipy.spatial.distance import euclidean
+from sklearn.cluster import KMeans as k_means
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+
+
+data = df['summary']
+
+# 预处理文本数据
+corpus = [text for text in data]
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(corpus)
+transformer = TfidfTransformer()
+tfidf = transformer.fit_transform(X)
+
+# 转换为稀疏矩阵
+kmeans_array = tfidf.toarray()
+
+# 计算损失函数值
+k_values = range(1, 10)
+losses = []
+
+for k in k_values:
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(kmeans_array)
+    losses.append(kmeans.inertia_)
+
+# 绘制曲线
+plt.plot(k_values, losses, marker='o')
+plt.xlabel('Number of Clusters (K)')
+plt.ylabel('Loss')
+plt.title('Elbow Method for Optimal K')
+plt.show()
+
+"""
