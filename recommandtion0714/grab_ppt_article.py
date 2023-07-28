4ffe2042430fa00e8d585f2cd7d19ea93fcd92ac
chickenmaru <105425398+chickenmaru@users.noreply.github.com>
2023-07-29 03:37:19 +0800

    Update grab_ppt_article.py
    將爬蟲ptt更新至資料庫

--- recommandtion0714/grab_ppt_article.py
+++ recommandtion0714/grab_ppt_article.py
@@ -9,11 +9,19 @@ import os
 import time
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
-import emotion 
+import datetime
+import mysql.connector
+#import emotion 
 
 current_dir = os.getcwd()
 driver_name = "chromedriver.exe"
 driver_path = current_dir+"\\"+driver_name
+
+host = 'localhost'
+user = 'root'
+password = '你的mysql密碼'
+database = 'mydb'
+charset =  "utf8"
 
 #這一邊要改成讀資料庫的 需要讀的是 ptt版面跟次主題的那個配對表
 def open_ptt_url():
@@ -22,13 +30,20 @@ def open_ptt_url():
     return df
 
 #這一邊要改成輸出到資料庫 需要輸出到 存放ptt內容的資料庫
-def output_ptt_data_to_database(sub_topics,titles,links,dates,page_indexes,emotion_scores):
+def transfer_dictionary(sub_topics,titles,links,dates,page_indexes,emotion_scores):
+    """
     data_list =[]
     for sub_topic,title,link,date,page_index,emotion_score in zip(sub_topics,titles,links,dates,page_indexes,emotion_scores):
         data_list.append([sub_topic,title,link,date,page_index,emotion_score])
     df_ptt_output = pd.DataFrame(data_list)
     print(data_list)
     df_ptt_output.to_excel("data/ptt_data.xlsx", index=False)
+    """
+    data_list = []
+    for sub_topic,title,link,date,page_index in zip(sub_topics,titles,links,dates,page_indexes):
+        data_list.append({'subtopic':sub_topic,'title':title,'link':link,'date':date,'page':int(page_index)},)
+    print(data_list)
+    return data_list
 
 #將ptt網址的字串進行處理
 def split_ptt_link(URL,already_get_index):
@@ -43,19 +58,52 @@ def split_ptt_link(URL,already_get_index):
         return True,page_index
     else:
         return False,page_index
-    
+"""  
 # 這一邊要改成讀資料庫的 需要讀的是 ptt版面跟次主題的那個配對表 更新ptt subtopic的配對表的index ***記得要等所有都做完才能更新
-def renew_ptt_subtopic_table_index(SUBTOPIC,URL,page_index):
-    data_list =[SUBTOPIC,URL,page_index]
+def output_ptt_data_to_database(subtopic,URL,page_index):
+    data_list =[subtopic,URL,page_index]
     return data_list
+""" 
 
-def renew_ptt_subtopic_table_to_database(data_list):
-    df_ptt_output = pd.DataFrame(data_list)
-    print(data_list)
-    df_ptt_output.to_excel("data/ptt.xlsx", index=False)
+def renew_ptt_subtopic_table_to_database(ptt_renewed_link_list):
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database, charset=charset)
+    cursor = connection.cursor()
+    for ptt_renewed_link in ptt_renewed_link_list:
+
+        # SQL更新语句
+        update_query = "UPDATE tb_ptt_search_link SET page = %s WHERE ptt_link_id = %s"
+
+        # 执行更新
+        data = (ptt_renewed_link['ptt_link_id'], ptt_renewed_link['page'])
+        cursor.execute(update_query, data)
+
+        # 提交更改
+        connection.commit()
+
+    # 关闭连接
+    connection.close()
 
 
-def grab_ptt_article_everyday():
+def transfer_date_type(date_str):
+    month, day = map(int, date_str.split('/'))
+    current_datetime = datetime.datetime.now()
+
+    # 从当前日期和时间中提取年份信息
+    current_year = current_datetime.year
+    # 使用指定的年份与月份、日期组合成新的日期
+    new_date = datetime.date(current_year, month, day)
+
+    # 将新的日期转换为字符串，格式为'YYYY-MM-DD'
+    new_date_str = new_date.strftime('%Y-%m-%d')
+
+    return new_date_str
+
+def renew_ptt_subtopic_table_index(ptt_link_id,subtopic_id,subtopic,URL,page_index):
+    dic = {'ptt_link_id':ptt_link_id,'subtopic_id':subtopic_id,'subtopic':subtopic,'ptt_url':URL,'page':page_index}
+    return dic
+
+
+def grab_ptt_article_everyday(df_ptt_subtopic):
     sub_topics=[]
     titles=[]
     links=[]
@@ -63,8 +111,8 @@ def grab_ptt_article_everyday():
     page_indexes=[]
     emotion_scores=[]
 
-    df_ptt_subtopic = open_ptt_url()
-    new_df_ptt_subtopic = []
+    #df_ptt_subtopic = open_ptt_url()
+    new_list_ptt_subtopic = []
     
     options = webdriver.ChromeOptions()  
     prefs = {'profile.default_content_setting_values':{'notifications': 2}}
@@ -72,7 +120,7 @@ def grab_ptt_article_everyday():
     options.add_argument("disable-infobars")
     driver = webdriver.Chrome(executable_path=driver_path,chrome_options=options)
 
-    for SUBTOPIC,URL,already_get_index in zip (df_ptt_subtopic['Sub_Topic'],df_ptt_subtopic['PTT_URL'],df_ptt_subtopic['PTT_Index']):
+    for ptt_link_id, subtopic_id, subtopic, URL, already_get_index in zip (df_ptt_subtopic['ptt_link_id'],df_ptt_subtopic['subtopic_id'],df_ptt_subtopic['subtopic'],df_ptt_subtopic['ptt_url'],df_ptt_subtopic['page']):
         count=1
         driver.get(URL)
         time.sleep(2)
@@ -88,22 +136,115 @@ def grab_ptt_article_everyday():
             elem_dates = driver.find_elements(By.CSS_SELECTOR, '#main-container > div > div> div> div.date')
             elem_links = driver.find_elements(By.CSS_SELECTOR, '#main-container > div> div> div.title > a')
             if(count==1):
-                new_df_ptt_subtopic.append(renew_ptt_subtopic_table_index(SUBTOPIC,URL,page_index))
+                new_list_ptt_subtopic.append(renew_ptt_subtopic_table_index(ptt_link_id,subtopic_id,subtopic,URL,page_index))
                 count+=1
-            print("這是關於"+SUBTOPIC+"的新聞：")
+            print("這是關於"+subtopic+"的新聞：")
             if(is_search_next_page==True):
                 for title,date,link in zip (elem_titles,elem_dates,elem_links):
-                    sub_topics.append(SUBTOPIC)
+                    sub_topics.append(subtopic)
                     titles.append(title.text)
                     links.append(link.get_attribute('href'))
-                    dates.append(date.text)
+                    transfer_date = transfer_date_type(date.text)
+                    dates.append(transfer_date)
                     page_indexes.append(page_index)
                     print("標題是"+title.text)
-                    emotion_scores.append(emotion.run_emotion_ptt(title.text))
+                    
 
-    output_ptt_data_to_database(sub_topics,titles,links,dates,page_indexes,emotion_scores)
-    renew_ptt_subtopic_table_to_database(new_df_ptt_subtopic)
+    datalist = transfer_dictionary(sub_topics,titles,links,dates,page_indexes,emotion_scores)
+    #將更新到的頁數傳回置資
 
-grab_ptt_article_everyday()
+    return datalist,new_list_ptt_subtopic
+
+def read_ptt_data():
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database, charset=charset)
+
+    cursor = connection.cursor()
+
+    # SQL查询语句
+    select_query = "SELECT * FROM tb_ptt_search_link"
+
+    # 执行查询
+    
+    cursor.execute(select_query)
+
+    # 获取所有结果
+    result = cursor.fetchall()
+
+    # 将结果转换为DataFrame
+    ptt_link_df = pd.DataFrame(result, columns=[desc[0] for desc in cursor.description])
+
+    print("我從資料庫把ptt_link的資訊抓出來了")
+
+    # 打印DataFrame
+    print(ptt_link_df)
+
+    # 关闭连接
+    connection.close()
+
+    return ptt_link_df
+    
+
+def update_ptt_data():
+
+    ptt_link_df = read_ptt_data()
+    
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database, charset=charset)
+
+    cursor = connection.cursor()
+    create_table_query = '''
+    CREATE TABLE IF NOT EXISTS `mydb`.`tb_ptt_data` (
+    `ptt_data_id` INT NOT NULL AUTO_INCREMENT,
+    `subtopic` VARCHAR(45) NOT NULL,
+    `title` VARCHAR(200) NOT NULL,
+    `link` VARCHAR(400) NOT NULL,
+    `date` DATE NOT NULL,
+    `page` INT NOT NULL,
+    PRIMARY KEY (`ptt_data_id`))
+    ENGINE = InnoDB;
+
+        CREATE TABLE IF NOT EXISTS `mydb`.`tb_ptt_search_link` (
+    `ptt_link_id` INT NOT NULL,
+    `subtopic_id` INT NOT NULL,
+    `ptt_url` VARCHAR(400) NOT NULL,
+    `subtopic` VARCHAR(45) NOT NULL,
+    `page` INT NOT NULL,
+    PRIMARY KEY (`ptt_link_id`))
+    ENGINE = InnoDB;
+
+        CREATE TABLE IF NOT EXISTS `mydb`.`tb_subtopic_id` (
+    `subtopic_id` INT NOT NULL,
+    `subtopic_value` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`subtopic_id`))
+    ENGINE = InnoDB;
+
+    '''
+
+    #測試
+    #cursor.execute(create_table_query)
+
+    messages,new_list_ptt_subtopic = grab_ptt_article_everyday(ptt_link_df)
+
+    for message in messages:
+        insert_query = '''
+            INSERT INTO tb_ptt_data (subtopic, title, link, date, page)
+            VALUES (%s, %s, %s, %s, %s)
+        '''
+        data = (message['subtopic'], message['title'], message['link'], message['date'], message['page'])
+
+        cursor.execute(insert_query, data)
+
+    connection.commit()
+
+    connection.close()
+
+    renew_ptt_subtopic_table_to_database(new_list_ptt_subtopic)
+
+#update_ptt_data()
 
 
+def read_ptt_link_search_data_from_excel():
+    ptt_link_df = read_ptt_data()
+    
+    connection = mysql.connector.connect(host=host, user=user, password=password, database=database, charset=charset)
+
+    cursor = connection.cursor()
